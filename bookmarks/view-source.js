5e016a3dac0732081afc45e8c7da9b68cd54da9a
Callionica <16260283+callionica@users.noreply.github.com>
2024-07-13 10:45:18 +0100

    basic

--- bookmarks/view-source.js
+++ bookmarks/view-source.js
@@ -1,7 +1,10 @@
 const name = "callionica.view-source";
 
 globalThis[name] = function viewSource() {
-    alert("view-source") ;   
+    const source = document.documentElement.outerHTML;
+    const body = document.body;
+    body.style = "font-size: 28px; color: black; background-color: beige; border: 0; margin: 0; padding: 2vw; overflow-y: scroll; overflow-x: hidden;";
+    body.innerText = source;
 };
 
 globalThis[name]();
