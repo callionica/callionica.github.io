d23288412e61ba834eb986cdf8814e92ecc2bb66
Callionica <16260283+callionica@users.noreply.github.com>
2024-07-18 16:34:06 +0100

    basics

--- bookmarks/edit-manifest.js
+++ bookmarks/edit-manifest.js
@@ -1,9 +1,25 @@
 const name = "callionica.edit-manifest";
 
-globalThis[name] = function editManifest() {
+globalThis[name] = async function editManifest() {
     const manifestLinks = [...document.querySelectorAll("link[rel='manifest']")];
-    const url = manifestLinks[0]?.href;
-    alert(url);
+    const manifestLink = manifestLinks[0];
+
+    const originalURL = manifestLink?.href;
+    
+    alert(originalURL);
+    
+    if (originalURL === undefined) { return; }
+
+    const response = await fetch(originalURL);
+    const json = await response.json();
+
+    const text = JSON.stringify(json, null, 2);
+    alert(text);
+
+    const blob = new Blob([text], { type: "application/manifest+json" });
+    const editedURL = URL.createObjectURL(blob);
+
+    manifestLink.href = editedURL;
 };
 
 globalThis[name]();
