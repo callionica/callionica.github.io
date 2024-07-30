63a815d693695f9294ee0e1c6010e957d04f63ff
Callionica <16260283+callionica@users.noreply.github.com>
2024-07-30 21:11:45 +0100

    ginola shyroscope

--- colors/colors.js
+++ colors/colors.js
@@ -21229,6 +21229,7 @@ export function toSoundKey(text) {
     [/c([eiy])/g, "s$1"],
     [/ch([aeiy])/g, "sh$1"],
     [/dg([eiy])/g, "sh$1"],
+    [/g([eiy])/g, "sh$1"],
     [/[st](i[oa])/g, "sh$1"],
     [/re$/g, "er"],
     [/y$/g, "e"],
